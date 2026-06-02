6eb3f9bcc21aa9177562aee44389ab8d854c4dbd
GautamBairagi <gauatambairagi22@gmail.com>
2026-06-02 17:28:48 +0530

    asd

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173','https://master-admin-kiaan.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
